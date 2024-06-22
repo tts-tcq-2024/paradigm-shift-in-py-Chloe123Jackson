376a72907b5cc71b7173f9b71ddbf1e4e87ee032
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-06-22 22:01:18 +0530

    indentation changed in line 5

--- check_limits.py
+++ check_limits.py
@@ -2,7 +2,7 @@ def battery_temp_is_ok(temperature):
   if temperature < 0 or temperature > 45:
     print('Temperature is out of range!')
     return False
-   return True
+  return True
 def battery_soc_is_ok(soc):
   if soc < 20 or soc > 80:
     print('State of Charge is out of range!')
