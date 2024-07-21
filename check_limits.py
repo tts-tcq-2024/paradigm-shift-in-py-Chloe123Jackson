e2c484795474afde5c3eb4c5fc6ea48a0a4f0d3d
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-22 01:18:20 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -23,27 +23,6 @@ def battery_is_ok(temperature, soc, charge_rate):
   Cr_ok=battery__cr_is_ok(charge_rate)
   return Battery_ok and Soc_ok and Cr_ok
 
-MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
-MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
-
-def print_colour_code_table():
-    table=[['Pair number','Major colour','Minor colour']]
-    pair_number=1
-    row=[]
-    # for major_colour in MAJOR_COLORS:
-    #     for minor_colour in MINOR_COLORS:
-    #         row.append(pair_number)
-    #         row.append(major_colour)
-    #         row.append(minor_colour)
-    #         table.append(row)
-    #         pair_number+=1
-    #         row=[]
-    #         print(table)
-    for row in table:
-        print('--------------------------------------------')
-        print('| {:^11} | {:^12} | {:^12} |'.format(*row))
-
-
 if __name__ == '__main__':
   assert(battery_is_ok(25, 70, 0.7) is True)
   assert(battery_is_ok(50, 85, 0) is False)
