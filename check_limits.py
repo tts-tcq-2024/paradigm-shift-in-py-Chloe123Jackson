ac137b0940f94b981d494a5acc66600cf31af490
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-28 18:44:43 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,23 +1,19 @@
 range_list=[[[0,2.251,42.749,45],[0,'Temperature near minimum','','Temperature near maximum']],[[20,24.1,75.9,80],[1,'Approaching discharge','','Approaching charge-peak']],[[0,0.0039,0.08],[2,'charge reat near minimum','','charge rate near maximum']]]
 mapping=['Temperature','SOC','Charge rate']
 def battery_temp_is_ok(temperature):
-  message=check_range(0,temperature)
+  message=deliver_range_message(0,temperature)
   return print_message(message)
   
 def battery_soc_is_ok(soc):
-  message=check_range(1,soc)
+  message=deliver_range_message(1,soc)
   return print_message(message)
   
 def battery__cr_is_ok(charge_rate):
-  message=check_range(2,charge_rate)
+  message=deliver_range_message(2,charge_rate)
   return print_message(message)
   
-def check_out_of_range(message):
-  if message in range(0,3):
-    return False
-  return True
 
-def check_range(index_number,variable_value):
+def deliver_range_message(index_number,variable_value):
   for x in range_list[index_number][0]:
     if variable_value<x:
         return range_list[index_number][1][range_list[index_number][0].index(x)]
