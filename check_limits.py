5b6d7c12f06010e91f89b0df0ae0094e7ab97216
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-28 18:40:23 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,19 @@
-range_list=[[[0,2.251,42.749,45],[0,'Temperature near minimum','','Temperature near maximum']],[[20,24.1,75.9,80],[0,'Approaching discharge','','Approaching charge-peak']],[[0,0.0039,0.08],[0,'charge reat near minimum','','charge rate near maximum']]]
-error_message="Out of range"
+range_list=[[[0,2.251,42.749,45],[0,'Temperature near minimum','','Temperature near maximum']],[[20,24.1,75.9,80],[1,'Approaching discharge','','Approaching charge-peak']],[[0,0.0039,0.08],[2,'charge reat near minimum','','charge rate near maximum']]]
+mapping=['Temperature','SOC','Charge rate']
 def battery_temp_is_ok(temperature):
   message=check_range(0,temperature)
-    print_message(message)
-    return False
-  return True
+  return print_message(message)
+  
 def battery_soc_is_ok(soc):
   message=check_range(1,soc)
-    print_message(message)
-    return False
-  return True
+  return print_message(message)
+  
 def battery__cr_is_ok(charge_rate):
   message=check_range(2,charge_rate)
-    print_message(message)
+  return print_message(message)
+  
+def check_out_of_range(message):
+  if message in range(0,3):
     return False
   return True
 
@@ -22,12 +23,17 @@ def check_range(index_number,variable_value):
         return range_list[index_number][1][range_list[index_number][0].index(x)])
         break
     else:
-        return error_message
+        return variable_value
         break
   
   
 def print_message(message):
-  print(message)
+  if message in range(0,3):
+    print(mapping[message], "Out of range")
+    return False
+  else:
+    print(message)
+    return True
   
 def battery_is_ok(temperature, soc, charge_rate):
   Battery_ok=battery_temp_is_ok(temperature)
