6dacb9400fea1f91a49e838a6a0334aa29b18997
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-28 16:50:06 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,21 +1,33 @@
+range_list=[[[0,2.251,42.749,45],['Temperature out of range','Temperature near minimum','','Temperature near maximum']],[[20,24.1,75.9,80],['Soc out of range','Approaching discharge','','Approaching charge-peak']],[[0,0.0039,0.08],['charge rate out of range','charge reat near minimum','','charge rate near maximum']]]
+error_message="Out of range"
 def battery_temp_is_ok(temperature):
-  if not 0<=temperature<=45:
-    print_message()
+  message=check_range(0,temperature)
+    print_message(message)
     return False
   return True
 def battery_soc_is_ok(soc):
-  if not 20<=soc<=80:
-    print_message()
+  message=check_range(1,soc)
+    print_message(message)
     return False
   return True
 def battery__cr_is_ok(charge_rate):
-  if charge_rate > 0.8:
-    print_message()
+  message=check_range(2,charge_rate)
+    print_message(message)
     return False
-
   return True
-def print_message():
-  print("Out of range")
+
+def check_range(index_number,variable_value):
+  for x in range_list[index_number][0]:
+    if variable_value<x:
+        return range_list[index_number][1][range_list[index_number][0].index(x)])
+        break
+    else:
+        return error_message
+        break
+  
+  
+def print_message(message):
+  print(message)
   
 def battery_is_ok(temperature, soc, charge_rate):
   Battery_ok=battery_temp_is_ok(temperature)
